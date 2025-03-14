0dd01796d81e0f7971fc4eddbee01904b96e891c
metauhid <alamcse19@gmail.com>
2025-03-14 14:48:11 +0530

    Updated songs folder and fixed fetch issue

--- js/music.js
+++ js/music.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds (seconds) {
 
 async function getSongs(folder){
     currFolder = folder
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
