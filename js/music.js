409a9a4da2429f31ede316db456aa4d6cd26b078
metauhid <alamcse19@gmail.com>
2025-03-14 14:36:47 +0530

    Updated songs folder and fixed fetch issue

--- js/music.js
+++ js/music.js
@@ -70,7 +70,7 @@ const playMusic = (track, pause=false)=> {
 }
 
 async function displayAlbums() {
-    let a = await fetch (`http://127.0.0.1:5500/songs/`)
+    let a = await fetch (`/songs/`)
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -81,7 +81,7 @@ async function displayAlbums() {
         const e = array[index];
         if(e.href.includes("/songs/")){
             let folder = e.href.split("/").slice(-2)[1]
-            let a = await fetch (`http://127.0.0.1:5500/songs/${folder}/info.json`)
+            let a = await fetch (`/songs/${folder}/info.json`)
             let response = await a.json();
             console.log(response)
             Cardcontainer.innerHTML += `<div data-folder="${folder}" class="card">
